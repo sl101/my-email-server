c15da2ab04c76100df835fd8838558a37f468e30
Slava <v.zhevaga@gmail.com>
2024-03-29 22:50:31 +0100

    uncomment server start

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ server.post("/email/feedback", async (req, res) => {
 	}
 });
 
-//server.listen(3000, () => {
-//	console.log("listening on port 3000");
-//});
+server.listen(3000, () => {
+	console.log("listening on port 3000");
+});
 
 //module.exports = server;
